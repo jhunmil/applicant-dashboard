0b548c9855c497dbdad5ca835589e93d90696c7d
unknown <jhunmil@higher-education-marketing.com>
2023-10-19 09:15:37 -0400

    add new message page.. Update dropdown menu add auto click on link click..Please update js and css

--- public/js/components.js
+++ public/js/components.js
@@ -3,33 +3,34 @@ const dropDownMenuApp = () => {
   // naviation dropdown
   let dropdownButton = document.querySelectorAll(".cc-nav-link");
   let dropdownMenu = document.querySelectorAll(".cc-dropdown-menu");
+  let allLink = document.querySelectorAll(".cc-dropdown-menu ul>li a,.cc-dropdown-menu ul>li button");
+  let isDropdownOpen = false;
 
-  let isDropdownOpen = false; 
 
- 
 
   dropdownButton.forEach((button, index) => {
     button.addEventListener("click", () => {
       isDropdownOpen = !isDropdownOpen;
-/*
-      if (isDropdownOpen) {
-        dropdownMenu[index].classList.remove("hidden");
-      
-    
-      } else {
-        dropdownMenu[index].classList.add("hidden");
-        isDropdownOpen = false; 
-      } */
+
       if (dropdownMenu[index].classList.contains('hidden')) {
         dropdownMenu[index].classList.remove('hidden');
-    } else {
-      dropdownMenu[index].classList.add('hidden');
-    }
-
-
+      } else {
+        dropdownMenu[index].classList.add('hidden');
+      }
 
     });
   });
+
+  //Close hide menu dropdown once link is clicked
+  allLink.forEach((link, index) => {
+    link.addEventListener("click", (event) => {
+      let dropdownMenu = link.closest(".cc-dropdown-menu");
+      dropdownMenu.classList.add('hidden');
+    });
+  });
+
+
+
 
   window.addEventListener("click", (event) => {
     dropdownButton.forEach((button, index) => {
@@ -42,6 +43,15 @@ const dropDownMenuApp = () => {
       }
     });
   });
+
+
+
+
+
+
+
+
+
 };
 
 
@@ -50,10 +60,10 @@ const dropdownMore = () => {
   // moreButton toggle
   let dropdownButton = document.querySelectorAll('.cc-moretoggle-btn');
   let dropdownMenu = document.querySelectorAll('.cc-more-toggle-content');
+  let allLink = document.querySelectorAll(".cc-more-toggle-content ul>li a,.cc-more-toggle-contentul>li button");
+  let isDropdownOpen = false;
 
-  let isDropdownOpen = false; 
 
- console.log(dropdownButton);
 
   dropdownButton.forEach((button, index) => {
 
@@ -62,14 +72,24 @@ const dropdownMore = () => {
 
       if (dropdownMenu[index].classList.contains('hidden')) {
         dropdownMenu[index].classList.remove('hidden');
-    } else {
-      dropdownMenu[index].classList.add('hidden');
-    }
+      } else {
+        dropdownMenu[index].classList.add('hidden');
+      }
 
 
 
     });
   });
+
+  
+ //Close hide menu dropdown once link is clicked
+ allLink.forEach((link, index) => {
+  link.addEventListener("click", (event) => {
+    let dropdownMenu = link.closest(".cc-more-toggle-content");
+    dropdownMenu.classList.add('hidden');
+  });
+});
+
 
   window.addEventListener("click", (event) => {
     dropdownButton.forEach((button, index) => {
@@ -82,9 +102,42 @@ const dropdownMore = () => {
       }
     });
   });
+
 };
 
 
+//show message Box for message.html
+const showMessageSection = () => {
+
+  let element = document.getElementById("cc-message-container"); // Replace with the ID of your element
+  element.classList.remove("hidden");
+
+
+}
+
+
+const disableMessageBox = () => {
+  let element = document.getElementById("cc-message-editor"); // Replace with the ID of your element
+  if (element && !element.classList.contains("disabled")) {
+    element.classList.add("disabled");
+    //  element.disabled = true;
+  }
+}
+
+const enableMessageBox = () => {
+
+
+  let element = document.getElementById("cc-message-editor");
+  element.classList.remove("disabled");
+
+  element.scrollIntoView();
+  element.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center'
+  });
+  return false;
+
+}
 
 
 const initModule = () => {
